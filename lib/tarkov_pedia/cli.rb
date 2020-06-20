383d7668a7f9e74d5abf1d6900b2cb4de634a6cd
ejguzman3988 <eriberto176@gmail.com>
2020-06-20 19:18:12 -0400

    Realized that building a menu class would serve the same funciton as the CLI class. Put back the code from the menu class into the CLI class and deleted the menu class.

--- lib/tarkov_pedia/cli.rb
+++ lib/tarkov_pedia/cli.rb
@@ -1,42 +1,43 @@
 class TarkovPedia::CLI
 
     def call
-        TarkovPedia::Menu.new
-        
-    end
-
-    def menu
-        
-        # puts "What would you like to search?"
-        
-        # puts <<-DOC
-        #     1. Items
-        #     2. Quests
-        # DOC
-
-        # interest = gets.chomp.downcase
-
-        # puts "What is the exact name of the #{user_select.delete_suffix('s')} you wish to search for?"
-        
-        # name = gets.chomp.downcase
-
-        # puts "Which process would you like to do?"
-
-        # puts <<-DOC
-        
-        # 1. description
-
-        # 2. price
-        
-        # 3. go back to menu 
-        
-        # DOC
-
-        # process = gets.chomp.downcase
-        
+        menu
         
     end
 
     
+        
+    def interest
+        puts <<-DOC
+        1. Items
+        2. Quests
+        DOC
+
+        interest = gets.chomp.downcase
+        interest
+    end
+
+    def name
+        puts "What is the exact name of the #{user_select.delete_suffix('s')} you wish to search for?"
+        
+        name = gets.chomp.downcase
+
+    end
+
+    def process
+            puts "Which process would you like to do?"
+
+        puts <<-DOC
+        
+        1. description
+
+        2. price
+        
+        3. Back to menu
+        
+        DOC
+
+        process = gets.chomp.downcase
+    end 
     
 end
